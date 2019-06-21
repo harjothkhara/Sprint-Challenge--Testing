bdce46f164ce8eac924342f98195d5c138a361f8
harjothkhara <harjoth.khara@gmail.com>
2019-06-21 09:21:23 -0700

    helper methods for router

--- router/router.spec.js
+++ router/router.spec.js
@@ -4,10 +4,10 @@ const db = require('../data/dbConfig.js');
 
 describe('server.js', () => {
     beforeEach(async () => {
-        await db("posts").truncate();
+        await db("games").truncate();
     });
-    describe('GET /games', async() => {
-        it('should return status code 200 OK', () => {
+    describe('GET /games', () => {
+        it('should return status code 200 OK', async () => {
             const response = await request(server).get('/api/games');
             expect(response.status).toBe(200)
         });
@@ -19,10 +19,12 @@ describe('server.js', () => {
             const response = await request(server).get('/api/games');
             expect(Array.isArray(response.body)).toBeTruthy();
         });
-    })
-    describe("POST /games", async() => {
+    });
+    describe("POST /games", () => {
         it('should return status code 201 if successful', async () => {
-            const response = await request(server).post('/api/games').send({
+            const response = await request(server)
+             .post('/api/games')
+             .send({
                 title: 'Pacman',
                 genre: 'Arcade',
                 releaseYear: 1980
@@ -30,28 +32,34 @@ describe('server.js', () => {
             expect(response.status).toBe(201);
         })
         it('should return json if successful', async () => {
-            const response = await request(server).post('/api/games').send({
+            const response = await request(server)
+              .post('/api/games')
+              .send({
                 title: 'Pacman',
                 genre: 'Arcade',
                 releaseYear: 1980
             });
             expect(response.type).toBe('application/json');
-        })
+        });
         it('should return created object if successful', async () => {
-            const response = await request(server).post('/api/games').send({
+            const response = await request(server)
+              .post('/api/games')
+              .send({
                 title: 'Pacman',
                 genre: 'Arcade',
                 releaseYear: 1980
             });
             expect(typeof response).toBe("Object");
             expect(response.title).toBe("Pacman")
-        })
+        });
         it('should fail with status code 422 if information is incomplete', async () => {
-            const response = await request(server).post('/api/games').send({
+            const response = await request(server)
+              .post('/api/games')
+              .send({
                 genre: 'Arcade',
                 releaseYear: 1980
             });
             expect(response.status).toBe(422)
-        })
-    })
+        });
+    });
 });
